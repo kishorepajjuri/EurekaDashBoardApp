a1122c7702fb35f00b07967aaa424f545480237e
kishore <kishorjava91@gmail.com>
2020-05-17 18:18:32 +0530

    changes

--- src/main/java/com/eureka/EurekaMainApp.java
+++ src/main/java/com/eureka/EurekaMainApp.java
@@ -2,11 +2,19 @@ package com.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaMainApp {
+public class EurekaMainApp extends SpringBootServletInitializer {
+ 
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(EurekaMainApp.class);
+    }
+ 
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaMainApp.class, args);
